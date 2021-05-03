fcb55499d0dc5b685f88d39bef61d895dd473950
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-05-03 11:19:20 +0900

    コミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   resources :microposts
   resources :users
-  root 'application#hello'
 end
 # コメント
